ca03aa8cc72b46bff15a868e76fc5ba4b3bfcea6
Hélvio Filho <helviosvf@gmail.com>
2022-04-07 11:41:33 -0300

    Prisma configured

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
+import { TestController } from './test.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
+  controllers: [TestController]
 })
 // eslint-disable-next-line prettier/prettier
 export class HttpModule { }

d55d7cff9d8401320353cea8e79c408833fb7230
Hélvio Filho <helviosvf@gmail.com>
2022-04-12 18:31:31 -0300

    server side cache

--- web/src/lib/withApollo.tsx
+++ web/src/lib/withApollo.tsx
@@ -1,23 +1,23 @@
-import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 import { NextPage } from "next";
 
 export const withApollo = (Component: NextPage) => {
   return function Provider(props: any) {
     return (
-      <ApolloProvider client={getApolloClient()}>
+      <ApolloProvider client={getApolloClient(props.apolloState)}>
         <Component {...props} />
       </ApolloProvider>
     )
   }
 }
 
-function getApolloClient() {
+function getApolloClient(ssrCache?: NormalizedCacheObject) {
   const httpLink = createHttpLink({
     uri: 'http://localhost:3332/graphql',
     fetch,
   });
 
-  const cache = new InMemoryCache();
+  const cache = new InMemoryCache().restore(ssrCache ?? {});
 
   return new ApolloClient({
     link: from([httpLink]),
